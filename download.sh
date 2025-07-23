9a4732768b1047b09232bd199d1313fe2fa8585a
Kirill Klenov <horneds@gmail.com>
2025-07-23 14:14:14 +0300

    refactor: reorder desktop icon creation in download script 🛠
    This commit refactors the `download.sh` script by reordering the creation of desktop icons. The uninstall icon is now created after the main SDWeak icon, which streamlines the logic and makes the script easier to follow. 👰

--- download.sh
+++ download.sh
@@ -8,17 +8,6 @@ wget https://github.com/Taskerer/SDWEAK/releases/latest/download/SDWEAK.zip
 unzip SDWEAK.zip
 
 # Create a desktop icons
-rm -rf "$HOME"/Desktop/SDWeakUninstall.desktop 2>/dev/null
-echo '#!/usr/bin/env xdg-open
-[Desktop Entry]
-Name=Uninstall SDWeak
-Exec=bash ${PREFIX}/SDWEAK/uninstall.sh
-Icon=delete
-Terminal=false
-Type=Application
-StartupNotify=false' >"$HOME"/Desktop/SDWeakUninstall.desktop
-chmod +x "$HOME"/Desktop/SDWeakUninstall.desktop
-
 rm -rf "$HOME"/Desktop/SDWeak.desktop 2>/dev/null
 echo "#!/usr/bin/env xdg-open
 [Desktop Entry]
@@ -29,5 +18,16 @@ Terminal=false
 Type=Application
 StartupNotify=false" >"$HOME"/Desktop/SDWeak.desktop
 chmod +x "$HOME"/Desktop/SDWeak.desktop
+
+rm -rf "$HOME"/Desktop/SDWeakUninstall.desktop 2>/dev/null
+echo '#!/usr/bin/env xdg-open
+[Desktop Entry]
+Name=Uninstall SDWeak
+Exec=bash ${PREFIX}/SDWEAK/uninstall.sh
+Icon=delete
+Terminal=false
+Type=Application
+StartupNotify=false' >"$HOME"/Desktop/SDWeakUninstall.desktop
+chmod +x "$HOME"/Desktop/SDWeakUninstall.desktop
 
 zenity --info --text="Download of SDWEAK has been completed!" --width=300
